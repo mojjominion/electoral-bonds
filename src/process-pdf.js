02519c4155a920ed3f1429cd67565aeddc6d3c7f
Mohan <mohan@stackbox.xyz>
2024-03-21 20:52:22 +0530

    feat: :sparkles: add new pdf files and reader for those

--- src/process-pdf.js
+++ src/process-pdf.js
@@ -1,11 +1,13 @@
 import { flush } from "./flush.js";
-import { reader } from "./reader.js";
+import { exhaustiveReader, reader } from "./reader.js";
 
 export async function processPdfs() {
-  const donners = await reader("input/donners.pdf");
-  const parties = await reader("input/parties.pdf", true);
+  // const donners = await reader("input/donners.pdf");
+  // const parties = await reader("input/parties.pdf", true);
+  // flush(donners, "donners");
+  // flush(parties, "parties");
 
-  flush(donners, "donners");
-  flush(parties, "parties");
+  const purchasers = await exhaustiveReader("input/purchasers.pdf");
+  flush(purchasers, "purchasers");
 }
 await processPdfs();
--- src/reader.js
+++ src/reader.js
@@ -43,3 +43,70 @@ export async function reader(filePath, isParty = false) {
 
   return rows;
 }
+
+const purchaserColumns = [
+  "serialNo",
+  "urn",
+  "journalDate",
+  "date",
+  "dateOfExpiry",
+  "donner",
+  "prefix",
+  "bondNumber",
+  "value",
+  "issueBranchCode",
+  "issueTeller",
+  "status",
+];
+
+function splitIndexes(data = [], start) {
+  const indexes = [];
+  for (let i = 0; i < data.length; i++) {
+    if (+data[i] == start + 1) {
+      indexes.push(i);
+      start++;
+    }
+  }
+  return indexes;
+}
+
+export async function exhaustiveReader(filePath) {
+  // Read the PDF file
+  const pdfData = new Uint8Array(readFileSync(filePath));
+  const rows = [];
+  let serialStart = 0;
+
+  try {
+    const pdf = await getDocument(pdfData).promise;
+    const numPages = pdf.numPages;
+
+    for (let pageNum = 1; pageNum <= numPages; pageNum++) {
+      const page = await pdf.getPage(pageNum);
+      const textContent = await page.getTextContent({
+        normalizeWhitespace: true,
+        // disableCombineTextItems: true,
+      });
+      const pageText = textContent.items
+        .filter((x) => !!x.str.toString().trim())
+        .map((x) => x.str);
+
+      const indexes = splitIndexes(pageText, serialStart);
+      serialStart = +pageText[indexes[[indexes.length - 1]]];
+
+      for (let j = 0; j < indexes.length; j++) {
+        const curIndex = indexes[j];
+        const nextIndex = indexes[j + 1];
+        const cells = pageText.slice(curIndex, nextIndex);
+        const obj = cells.reduce((acc, cur, index) => {
+          purchaserColumns[index] && (acc[purchaserColumns[index]] = cur);
+          return acc;
+        }, {});
+        rows.push(obj);
+      }
+    }
+  } catch (error) {
+    console.error("[PDF Error]:", error);
+  }
+
+  return rows;
+}
